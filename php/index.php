163897149d66171def025e49e652ef4b6474b374
beammy <leothai.beammy@gmail.com>
2023-03-17 10:03:53 +0700

    New learn PHP sentence

--- php/index.php
+++ php/index.php
@@ -1,2 +1,22 @@
-Hello Morning
+<?php
+$money = "1000";
+echo "My money is ". $money . "<br>";
+echo "My house is ". $color . "<br>";
+echo "My boat is ". $color . "<br>";
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h4>" . $txt1 . "</h4>";
+echo "Study PHP at " . $txt2 . "<br>"; 
+echo $x + $y;
+?>
+
+
+
+
+
+
 
