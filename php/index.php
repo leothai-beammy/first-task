9bd2d43c6e5b706f95e9484f4da17b766e8cd601
beammy <leothai.beammy@gmail.com>
2023-03-25 10:02:45 +0700

    Study 25 March 2023

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <b>Beammy Study List</b> <br>
 <ul>
     <li>24 March 2023 : <a href="view_for.php">View about 'for'</a></li>
-    <li></li>
+    <li>25 March 2023v: <a href="view_for2.php">View about 'for2'</a></li>
 </ul>
 
 
---
+++ php/view_for2.php
@@ -0,0 +1,2 @@
+<?php
+show_source("./Learn_for2.php");
