b456ae9577c4d2e4d83f047dcfaaba3a0bd7459d
beammy <leothai.beammy@gmail.com>
2023-03-26 08:43:04 +0700

    Study 26 March 2023

--- php/26032023_for.php
+++ php/26032023_for.php
@@ -1,2 +1,14 @@
 <?php
-show_source("./26032023_for.php");
+
+for ( $a = 1 ; $a <= 10 ; $a++ ) {
+    echo " 2* $a = " . $a * 2 . "<br>"; 
+}
+echo "<br>";
+
+for ( $a = 2 ; $a <=4 ; $a++ ) {
+    for ( $b = 1 ; $b <=9 ; $b++ ) {
+        echo "$a * $b = " . $a * $b . "<br>";
+    }
+    echo "<br>"; 
+}
+echo "<br>";
--- php/index.php
+++ php/index.php
@@ -1,7 +1,9 @@
 <b>Beammy Study List</b> <br>
 <ul>
     <li>24 March 2023 : <a href="view_for.php">View about 'for'</a></li>
-    <li>25 March 2023v: <a href="view_for2.php">View about 'for2'</a></li>
+    <li>25 March 2023: <a href="view_for2.php">View about 'for2'</a></li>
+    <li>26 March 2023 : <a href="26032023_for.php">View about 'for'</a></li>
+    
 </ul>
 
 
