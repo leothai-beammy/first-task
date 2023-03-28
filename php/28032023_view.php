ead3cdaa662be0f3728b6d9a096e3666e47408cb
beammy <leothai.beammy@gmail.com>
2023-03-28 08:03:47 +0700

    28032023_for.php

---
+++ php/28032023_view.php
@@ -0,0 +1,2 @@
+<?php
+show_source("28032023_view.php");
--- php/index.php
+++ php/index.php
@@ -4,6 +4,9 @@
     <li>25 March 2023: <a href="view_for2.php">View about 'for2'</a></li>
     <li>26 March 2023 : <a href="26032023_view.php">View about 'for'</a></li>
     <li>27 March 2023 : <a href="27032023_view.php">View about 'for'</a><li>
+    <li>28 March 2023 : <a href= "28032023_view.php">View about 'for'</a><li>
+
+
 </ul>
 
 
